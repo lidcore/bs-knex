0fd02067e7952f9d86c80bd568364203458e4409
Romain Beauxis <toots@rastageeks.org>
2018-04-12 11:34:10 -0500

    Better this way.

--- src/knex.ml
+++ src/knex.ml
@@ -1,5 +1,5 @@
 type instance
-type client = string -> instance [@bs]
+type client = string -> instance
 
 type config = [
   | `Pg of Pg.config
@@ -26,6 +26,6 @@ end
 
 module Make(Config:Config_t) = struct
   type t = instance
-  let knex name = Config.client name [@bs]
+  let knex = Config.client
   external where_ : 'a Js.t -> t = "where" [@@bs.send.pipe: t]
 end
